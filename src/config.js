1372a31df3dbea45bdf78c46e80db5dad3554a10
tarajeanne <t.jeanne.patel@gmail.com>
2019-10-23 14:52:18 -0500

    updated api to lambda

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://node-express-env.cseqbqgbij.us-east-2.elasticbeanstalk.com/api',
+  API_ENDPOINT: 'https://o6auc6lnp5.execute-api.us-east-1.amazonaws.com/dev/api',
 /*   API_ENDPOINT: 'http://localhost:8000/api',
  */  TOKEN_KEY: 'rambo-tracker-auth-token',
 }
