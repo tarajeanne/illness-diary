e809d3c4c7a8cb0c116c19c4cb1c1935e28a036d
tarajeanne <t.jeanne.patel@gmail.com>
2019-10-23 11:14:10 -0500

    changed to https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://node-express-env.cseqbqgbij.us-east-2.elasticbeanstalk.com/api',
+  API_ENDPOINT: 'https://node-express-env.cseqbqgbij.us-east-2.elasticbeanstalk.com/api',
 /*   API_ENDPOINT: 'http://localhost:8000/api',
  */  TOKEN_KEY: 'rambo-tracker-auth-token',
 }
