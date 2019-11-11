ad80a39b9d7e35679f7aa354cdbb4bc8f9c04105
tarajeanne <t.jeanne.patel@gmail.com>
2019-11-10 22:08:48 -0600

    switched back to heroku server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://o6auc6lnp5.execute-api.us-east-1.amazonaws.com/dev/api',
+  API_ENDPOINT: 'https://rambo-tracker.herokuapp.com/api',
 /*   API_ENDPOINT: 'http://localhost:8000/api',
  */  TOKEN_KEY: 'rambo-tracker-auth-token',
 }
